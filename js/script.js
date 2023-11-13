3381039e85e1c83942d9368e4c402195f3702a7c
devsmostafiz <mostafijurrhaman87@gmail.com>
2023-11-13 22:50:58 +0600

    update slik

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+      
+  $(function(){
+    
+    $('.client_slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:500,
+        dots:true,
+    });
+  })
