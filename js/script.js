2531025f05339e8c1f60a089e9f97f3014851fbf
devsmostafiz <mostafijurrhaman87@gmail.com>
2023-11-14 16:56:18 +0600

    last update

--- js/script.js
+++ js/script.js
@@ -9,7 +9,78 @@
         slidesToScroll: 1,
         arrows:false,
         autoplay:true,
-        autoplaySpeed:500,
+        autoplaySpeed:1000,
         dots:true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
     });
+
+   /* ========Shoes Brand Area ======= */
+
+
+    $('.brand_wrapper').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      arrows:false,
+      autoplay:true,
+      autoplaySpeed:1000,
+      dots:false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            infinite: true,
+            dots:false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots:false,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots:false,
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+  });
   })
